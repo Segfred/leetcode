10534955fda979f5f9baf18462e06c20cc5bc47c
Segfred <1104334344@qq.com>
2017-05-30 20:28:42 +0900

    sliding window maximum

---
+++ sldWindowMax.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+if(nums.size()<k) return {};
+vector<int> res{};
+deque<int> deq{};
+for(int i=0;i<(int)nums.size();++i){
+if(!deq.empty()&&deq.back()==i-k) deq.pop_back();//也可以先判断是否窗口满了，这样不会超过size，并且更快
+while(!deq.empty()&&nums[i]>=nums[deq.front()]) deq.pop_front();//是和nums[front]比较，不是和front比较，注意存的是下标！
+deq.emplace_front(i);//存的是下标，不是数本身，先存入新数，再判断窗口是否满了
+if(i>=k-1) res.emplace_back(nums[deq.back()]);//存到结果的是nums[]下标，不是下标
+}        
+return res;
+    }
+};
