69a6393410fede2fed4f216b2763eefd963870fc
Segfred <1104334344@qq.com>
2017-06-02 19:53:49 +0900

    Update groupAnagram.cpp

--- groupAnagram.cpp
+++ groupAnagram.cpp
@@ -9,8 +9,8 @@ string cur=strs[i];
 sort(cur.begin(),cur.end());
 hash[cur].emplace_back(i);
 }
-for(auto &ele:hash){
-vector<string> item{};
+for(auto &ele:hash){//遍历hash容易把所有相同string放到同一个vector，直接放结果里，遍历原来的strs就要一次次找对应
+vector<string> item{};//string位置，比较麻烦
 for(size_t idx:ele.second) item.emplace_back(strs[idx]);//存的是下标，不是字符串本身
 res.emplace_back(item);
 }
