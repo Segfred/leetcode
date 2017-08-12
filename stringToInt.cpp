040c151ccabc6faf33ac608ec4f8ea45003ca940
Segfred <1104334344@qq.com>
2017-08-12 17:36:17 +0900

    Update stringToInt.cpp

--- stringToInt.cpp
+++ stringToInt.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int myAtoi(const string &str) {
-if(str.empty()) return 0;
-const int len=(int)str.length();
-int sign=1, i=0, res=0;
-while(i<len&&str[i]==' ') ++i;//i<len别忘了
-if(str[i]=='+'||str[i]=='-') if(str[i++]=='-') sign=-1;
-while(i<len&&str[i]>='0'&&str[i]<='9' ){
-if(res>INT_MAX/10||res==INT_MAX/10&&str[i]>'7') return sign==-1? INT_MIN?INT_MAX;
-res=10*res+str[i++]-'0';
-}     
-return res*sign;
+    int myAtoi(string &str) {
+int i=0, sign=1, res=0;
+const int len=str.length();
+while(i<len&&str[i]==' ') ++i;//needs handling
+if(str[i]=='+'||str[i]=='-') if(str[i++]=='-') sign=-1;//别忘了前进i
+while(i<len){
+if(str[i]<'0'||str[i]>'9') break;
+if(res>INT_MAX/10||res==INT_MAX/10&&str[i]>'7') return sign==1? INT_MAX:INT_MIN;//234567,7在最后一位，前面已经等于23456的时候相等
+res=res*10+str[i++]-'0';//别忘了前进i
+}
+
+//if(res==INT_MAX&&sign==-1) return INT_MIN;//这样最小的数会当成溢出是错的
+return  res*sign;
     }
 };
