ebae484458b9776e5384f14b52b74cf6ae3bf412
Segfred <1104334344@qq.com>
2017-08-27 03:43:27 +0900

    Update diameterOfBiTree.cpp

--- diameterOfBiTree.cpp
+++ diameterOfBiTree.cpp
@@ -24,5 +24,5 @@ int res=max(postHeight(pnode->left,maxh), postHeight(pnode->right,maxh))+1;
 umap[pnode].emplace_back(res);
 return res;
 }
-unordered_map<TreeNode*, vector<int>> umap;
+unordered_map<TreeNode*, vector<int>> umap;//这里放vector纯粹为了省时间复杂度，实际上只需要放一个元素即可
 };
